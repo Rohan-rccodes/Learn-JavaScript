91f083b55e3d0e0b34cbf37dbd96e2db7249472a
Rohan Chauhan <140747527+Rohan-rccodes@users.noreply.github.com>
2024-02-01 11:35:25 +0530

    variable-Types

---
+++ basics/variable.js
@@ -0,0 +1,24 @@
+// const is a static variable cannot be re-assign
+const id = "123456";
+
+// let is most use variable type
+let fname = "rccodes"
+
+let email = "testmail@gmail.com"
+
+// var is also used same as let variable 
+var pswd = "123456"
+
+//empty variable 
+let cityname;
+
+console.log(id);
+console.log(fname);
+console.log(email);
+console.log(pswd);
+
+console.log(cityname)
+
+
+//console.table -- print the all data in tabular format
+console.table([id, fname, email, pswd, cityname])
