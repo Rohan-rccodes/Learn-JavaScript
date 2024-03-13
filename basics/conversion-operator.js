c63edfe4c6d98ed0f49cf183f76d069d68e9b138
Rohan Chauhan <140747527+Rohan-rccodes@users.noreply.github.com>
2024-03-13 20:19:28 +0000

    data-Conversion

---
+++ basics/conversion-operator.js
@@ -0,0 +1,33 @@
+let score = false 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//Notes ::
+
+// "33" =>  33
+//"33abc"  => NaN
+//true => 1;
+//false => 0
+
+let isLoggedIn = "Hitesh"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+// 1 = true;
+// "" => false;
+//"hitesh " => true 
+
+let Snumber = 33
+let StringNumber =String(Snumber)
+console.log( StringNumber);
+
+console.log(typeof StringNumber);
+
