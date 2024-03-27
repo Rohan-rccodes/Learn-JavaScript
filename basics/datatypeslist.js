01b238cd0eb6360760f1ab6c86dd64490fba9e9e
Rohan Chauhan <140747527+Rohan-rccodes@users.noreply.github.com>
2024-03-27 07:44:25 +0000

    datatype end

---
+++ basics/datatypeslist.js
@@ -0,0 +1,45 @@
+// Primitive
+
+// 7 TYPES : string, Number, Boolean, null, undefined, Symbol, BigInt;
+
+
+let usermail;
+const id ='15551'
+const score = 100; 
+const scoreValue = 100.5;
+
+const isloggedIn= false
+const outsideTemp = null
+
+const id2 = Symbol('123')
+const bigNumber = 123123212321231
+
+
+
+// Refference Types ( 3 Non-Primitive)
+// Array,
+// Objects
+// functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]
+let obj1 = 
+{
+    name: "codetime",
+    age: 22,
+
+}
+
+const myFunction = function()
+{
+    console.log("Hello new funtion")
+}
+
+console.log(typeof id)
+console.log(typeof id2)
+console.log(typeof score)
+console.log(typeof scoreValue)
+console.log(typeof isloggedIn)
+console.log(typeof outsideTemp)
+console.log(typeof bigNumber)
+console.log(typeof null)
+
