cc260b635bd84128a6435d450b6d3bd3be3e6b42
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:39:02 +0530

    q2 loop

---
+++ loops/q2_loop.js
@@ -0,0 +1,10 @@
+
+        //PRINT ALL THE EVEN NUMBERS UPTO 100;
+
+        for (let num = 0; num <= 100; num++) {
+            if (num % 2 === 0) {
+                console.log("Even Num : ", num)
+            }
+        }
+
+
