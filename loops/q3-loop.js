9591b8aa070c7e491071cf0329edac4957acbcbf
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:39:21 +0530

    q3 loop

---
+++ loops/q3-loop.js
@@ -0,0 +1,12 @@
+
+
+        //PRINT ALL THE ODD NUMBER UPTO 100;
+
+        for (num = 0; num <= 100; num++) {
+
+            if (num % 2 !== 0)// '!' not operator
+            {
+                console.log("Odd num : ", num);
+
+            }
+        }
