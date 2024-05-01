8db40d84870c3b7c89937aabbf2117b8a2e1922b
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:40:15 +0530

    while loop js

---
+++ loops/while_loop.js
@@ -0,0 +1,10 @@
+
+
+        //print 1 to 5 
+
+
+        let i = 0;
+
+        while (i <= 5) {
+            console.log("i = ", i);
+        }
---
+++ loops/whle_loop2.js
@@ -0,0 +1,15 @@
+
+        /*
+
+        while(){
+          //do the work
+        }
+        */
+
+        let i = 1;
+
+        while (i < 5) {
+          
+            console.log("While Loop")
+
+        }
