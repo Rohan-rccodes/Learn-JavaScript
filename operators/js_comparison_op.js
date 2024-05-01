f5da93a01c1aba92d9abc6caa52f16a50762affd
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:43:51 +0530

    js comaprison

---
+++ operators/js_comparison_op.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Comaprison OPerator</title>
+    </head>
+
+    <body>
+
+    </body>
+    <script>
+        let a = 5;
+        let b = 2;
+
+        console.log('comparing the values of a & b: ');
+        console.log('a = b ', a == b);
+        console.log('a != b ', a != b);
+
+
+        console.log('Greater than & equals to :');
+        console.log('a > b ', a > b);
+        console.log('a >= b ', a >= b);
+        console.log('a < b ', a < b);
+        console.log('a <= b ', a <= b);
+
+        console.log('Js provide scrict comparison operators :');
+
+        console.log('a === b ', a === b);
+        console.log('a !== b ', a !== b);
+
+        let c = 5;
+
+        console.log('a == c', a==c);
+
+    </script>
+
+</html>
