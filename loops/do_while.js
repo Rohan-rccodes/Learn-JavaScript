89e0ff80d4fa92317e8f277b2bd641df77d8fb32
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:35:36 +0530

    do while js

---
+++ loops/do_while.js
@@ -0,0 +1,20 @@
+/*  example of while looop.
+    let i = 1;
+            while(i <= 10)
+            {
+                console.log("test for while loop");
+            }
+    
+     */
+
+    //DO WHILE LOOP
+
+
+
+    let i = 20
+
+
+        do {
+            console.log("do while loop in js");
+        }
+        while (i <= 20);
