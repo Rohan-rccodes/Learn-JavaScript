d0c08012fc45d1650fb7767f58a3cff3832fdd2c
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:37:43 +0530

    infinity loop js

---
+++ loops/infinite_loop.js
@@ -0,0 +1,14 @@
+
+// infinite loop is bad ways to use it
+            // WARNING DO NOT USE INFINITY LOOPS //
+
+            // its for example only 
+
+            //System crash
+            // it cannot stop until you shut down your computer: 
+
+            for (let i = 0; i >= 0; i++) {
+                console.log("i = ", i);
+            }
+
+            //run at your risk ☠️☠️
