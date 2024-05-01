ac7f74ed7be767ab2446212d7345d8eaab78518a
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:39:56 +0530

    sum loop js

---
+++ loops/sum_loop.js
@@ -0,0 +1,15 @@
+
+        //print the sum of 1 to 10
+
+
+        let sum = 0;
+            let n = 50;
+
+            for (let i = 1; i <= n; i++) {
+            sum = sum + i; // sum = 15
+        }
+        console.log("sum = ", sum)
+
+        console.log("loop has ended")
+
+
