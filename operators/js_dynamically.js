00ee7e9177b8a44343e180b40508590e5da69dd7
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:44:23 +0530

    dynamically js

---
+++ operators/js_dynamically.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamicak js</title>
+</head>
+<body>
+    
+</body>
+<script>
+    console.log('JavaScript is dynamically type language because js allow to create or write vaiables with defining data types for the vaiables js identify automatically while running the code');
+
+    age = 25;
+
+    console.log('for example : age in variale with carry value 25 js automatically identify in datatype');
+
+    console.log('Age is '+age)
+
+
+
+</script>
+</html>
