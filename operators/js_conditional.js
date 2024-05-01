c7ba927094fa06fa81baa98d0b63e5b25107843b
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:44:01 +0530

    js condition op

---
+++ operators/js_conditional.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>js_conditional Operator</title>
+    </head>
+
+    <body>
+
+    </body>
+    <script>
+
+        let mode = "dark";
+        let color;
+
+            if (mode === "dark") {
+
+            color = "black"
+        }
+            else (mode === "light")
+            {
+            color = "white";
+        }
+        console.log(color);
+
+
+    let num = 20;
+            console.log("Checking the number is ODD or Even");
+
+    console.log(num);
+
+    if (num % 2 === 0) {
+        console.log("even number");
+    }
+    else {
+        console.log("odd number");
+    }
+
+
+
+    let age = 20;
+    age >= 18 ? "adult" : "not adult"
+    {
+        console.log(age)
+    }
+
+
+    let num1;
+    console.log("checking the bnumber enter by user by is multiple of number:");
+    if(num1 === 0)
+    {
+        console.groupEnd
+    }
+    </script>
+
+</html>
