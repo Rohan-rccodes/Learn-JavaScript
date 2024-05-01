a307cbbf14a9b3a1831590e91b5a0791feb6f4fd
rccdoes <itschauhan101@gmail.com>
2024-05-01 19:44:09 +0530

    js logical op

---
+++ operators/js_logical_op.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>js Logical Operator</title>
+</head>
+<body>
+    
+</body>
+<script>
+    let a = 5;
+    let b = 2;
+    let c = 3;
+
+    let res1 = a>b; //true
+    let res2 = a<b; //false
+    let res3 = a==b; //false
+    let res4 = a==b; //false
+    let res5 = a==b; //false
+
+    console.log('res1 && res2', res1 && res2);
+    console.log('res1 && res3', res1 && res2);
+    console.log('res1 && res2', res1 || res2);
+    console.log('res1 && res3', res1 || res2);
+
+
+</script>
+</html>
