72fa00820a28de4f1a461fe969482fbeb51494e4
rccdoes <itschauhan101@gmail.com>
2024-05-03 19:14:35 +0530

    string literals

---
+++ strings/template_Literals.js
@@ -0,0 +1,15 @@
+
+//Templete Literals
+
+    //TEMPLATE LITERALS 
+
+        // IT'S A SPEACIAL TYPE OF STRING CREATED USING BACK TICKS
+
+        let str = "JavaScript";
+
+        let str2 = 'Learning';
+
+        let sentence = `This is a Template Literal`; //Speacial Literal
+
+        console.log(sentence);
+
